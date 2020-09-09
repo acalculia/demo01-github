04f741c663b74e812d7fa6c4e2079495679a97a5
acalculia <2397070892@qq.com>
2020-09-09 19:21:34 +0800

    新创建的项目

---
+++ demo01-github/src/main/java/com/baidu/Demo01GithubApplication.java
@@ -0,0 +1,13 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01GithubApplication.class, args);
+    }
+
+}
---
+++ demo01-github/src/main/java/com/baidu/utils/Demo01.java
@@ -0,0 +1,7 @@
+package com.baidu.utils;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("AAA");
+    }
+}
